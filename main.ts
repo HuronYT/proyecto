a2073d9d27d218287c324cb8c2fc59a6c35e6214
HuronYT <88743634+HuronYT@users.noreply.github.com>
2021-10-07 14:53:23 -0300

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,15 @@
+music.playMelody("- C5 B - - - - - ", 120)
+led.plot(2, 3)
+basic.showLeds(`
+    . . . . .
+    . # . # .
+    . # . # .
+    # . . . #
+    . # # # .
+    `)
+basic.forever(function () {
+	
+})
+basic.forever(function () {
+	
+})
